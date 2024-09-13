728572a950e37d037cf0784e5f29fb6e5b15610a
anthony <b09902033@csie.ntu.edu.tw>
2024-09-13 13:33:01 +0800

    docs: Add more description

--- README.md
+++ README.md
@@ -1,22 +1,49 @@
 # Video Face Detection
 
-Features:
-- Design for large amount of videos (>1k).
+The code is designed for large amount of videos (>1k).
+
+## TODOs
+
+- [ ] optimize: accelerate frame sampling process
 
 ## Start
 
 ```bash
 pip install -r requirements.txt
-bash run.sh [folder-to-videos]
+bash run.sh [video_folder]
 ```
 
 ## Implementation
 
-1. Extract 100 frames as png from 1k mp4 files.
-2. Detect all faces within each frame with `face_recognition.batch_face_locations` (require GPU)
-3. Calculate its embedding with `face_recognition.face_encodings`.
+1. Extract 100 frames as png from all the mp4 files in `video_folder`.
+2. Detect all faces in each frame with `face_recognition.batch_face_locations` (GPU required)
+3. Encode the faces with `face_recognition.face_encodings`.
 4. Cluster all the faces so that the distance between any two vectors within the same cluster is less than or equal to `TOLERANCE`
-5. Dump the following info into `results.json`:
-    1. % of frames with faces
-    2. % of frequency for each faces
+5. Dump the results to `results.json`.
+
+### results.json
+Format in results.json:
+```python
+[
+    video_id,
+    face_prob,
+    face_clusters,
+    avg_num_faces
+]
+```
+
+$N$: number of sampled frames
+$n$: number of frames that has at least one face
+$a_i$: number of faces in frame $i$
+$b_i$: number of faces in frame $i$ after background faces removal
+$c_i$: for a particular face $i$, it appears $c_i$ time in all the $N$ frames
+
+* `video_id`: the folder name in `frames/`
+* `face_prob`: the probability $\frac{n}{N}$
+* `face_clusters`: the distribution of each faces: $\frac{c_1}{N}, \frac{c_2}{N}, ...$
+* `avg_num_faces`: the average of faces number for all frames after background faces removal: $\frac{\sum_i^n b_i}{n}$
+
+### `LOG_CLUSTER_IMG`
+
+Save the face clustering results to `debug/` folder. This is used to tune the `TOLERANCE`.
 
--- main.py
+++ main.py
@@ -10,19 +10,19 @@ from scipy.spatial.distance import pdist
 from collections import Counter
 
 
-TOLERANCE = 0.5
+TOLERANCE = 0.7
 LOG_CLUSTER_IMG = False
 BATCH_SIZE = 30
 
-def find_face_in_pool(face_emb, emb_pool, TOLERANCE=0.4):
-    """ 
-    Return the pos of the first match, otherwise return -1.
-    """
-    distances = list(face_recognition.face_distance(emb_pool, face_emb))
-    distances = [dis if dis <= TOLERANCE else 1 for dis in distances]
-    if not distances or min(distances) == 1:
-        return -1
-    return np.argmin(distances)
+# def find_face_in_pool(face_emb, emb_pool, TOLERANCE=0.4):
+#     """ 
+#     Return the pos of the first match, otherwise return -1.
+#     """
+#     distances = list(face_recognition.face_distance(emb_pool, face_emb))
+#     distances = [dis if dis <= TOLERANCE else 1 for dis in distances]
+#     if not distances or min(distances) == 1:
+#         return -1
+#     return np.argmin(distances)
 
 
 if __name__ == "__main__":
@@ -39,23 +39,43 @@ if __name__ == "__main__":
         all_faces = []
         has_face = 0
         batch_img = []
+        face_in_frames = []
+
+        # Read images
         for frame_png in os.listdir(os.path.join(frame_dir, video_folder)):
             image = face_recognition.load_image_file(os.path.join(frame_dir, video_folder, frame_png))
             image = np.ascontiguousarray(image[:, :, ::-1])
             batch_img.append(image)
 
-        # GPU
+        # Find face locations with GPU
         batch_of_face_locations = []
         for batch_i in range(0, len(batch_img), BATCH_SIZE):
             end = min(len(batch_img), batch_i+BATCH_SIZE)    
             locations = face_recognition.batch_face_locations(batch_img[batch_i:end], number_of_times_to_upsample=0)
             batch_of_face_locations.extend(locations)
 
+        # Encode the images into embedding
         for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
+            face_areas = [(pos[2]-pos[0])*(pos[1]-pos[3]) for pos in face_locations]
+            face_areas = sorted(face_areas, reverse=True)
+
+            # Background faces removal
+            # Definition: the face area is more than 3x smaller than
+            # the smallest face in the detected faces.
+            end_idx = len(face_areas)
+            for i in range(len(face_areas)):
+                if i+1 < len(face_areas) and face_areas[i] / face_areas[i+1] > 3:
+                    end_idx = i+1
+                    break
+            face_areas = face_areas[:end_idx]
+
+            # Encode faces into embeddings
             face_embs = face_recognition.face_encodings(
                 batch_img[frame_number_in_batch], known_face_locations=face_locations, num_jitters=10,
                 model='large'
             )
+            if len(face_areas) > 0:
+                face_in_frames.append(len(face_areas))
             all_embs.extend(face_embs)
 
             if LOG_CLUSTER_IMG:
@@ -66,7 +86,7 @@ if __name__ == "__main__":
             if len(face_embs) > 0:
                 has_face += 1
                 
-        
+        # Run clustering
         distance_matrix = pdist(all_embs, metric='euclidean')
         Z = linkage(distance_matrix, method='complete')
         clusters = fcluster(Z, t=TOLERANCE, criterion='distance')
@@ -81,7 +101,7 @@ if __name__ == "__main__":
                 cv2.imwrite(f"debug/{cluster}/{i}.png", all_faces[i])
 
         results.append([
-            video_folder, round(has_face/N, 2), [round(counter[i]/N, 2) for i in order_id]
+            video_folder, round(has_face/N, 2), [round(counter[i]/N, 2) for i in order_id], np.mean(face_in_frames)
         ])
         with open('results.json', "w") as fout:
             json.dump(results, fout, indent=2, ensure_ascii=False)
